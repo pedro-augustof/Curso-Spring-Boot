52a42fe60fd5198d6d34fb28843ee95b5a853c41
Pedro Augusto <pedro4032@hotmail.com>
2022-06-09 23:26:45 -0300

    Implementing JPA

--- Vendas/src/main/java/com/udemy/cursospring/Vendas/VendasApplication.java
+++ Vendas/src/main/java/com/udemy/cursospring/Vendas/VendasApplication.java
@@ -1,17 +1,52 @@
 package com.udemy.cursospring.Vendas;
 
+import com.udemy.cursospring.Vendas.domain.entity.Cliente;
+import com.udemy.cursospring.Vendas.domain.repositorio.Clientes;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
 
 @SpringBootApplication
-@RestController
 public class VendasApplication {
 
-	@GetMapping("/hello")
-	public String hellowWorld(){
-		return "hello world";
+	@Bean
+	public CommandLineRunner init(@Autowired Clientes clientes){
+		return args -> {
+			System.out.println("Salvando clientes");
+			clientes.salvar(new Cliente("Dougllas"));
+			clientes.salvar(new Cliente("Outro Cliente"));
+
+			List<Cliente> todosClientes = clientes.obterTodos();
+			todosClientes.forEach(System.out::println);
+
+			System.out.println("Atualizando clientes");
+			todosClientes.forEach(c -> {
+				c.setNome(c.getNome() + " atualizado.");
+				clientes.atualizar(c);
+			});
+
+			todosClientes = clientes.obterTodos();
+			todosClientes.forEach(System.out::println);
+
+			System.out.println("Buscando clientes");
+			clientes.buscarPorNome("Cli").forEach(System.out::println);
+
+			System.out.println("Deletando clientes");
+			clientes.obterTodos().forEach(c -> {
+				clientes.deletar(c.getId());
+			});
+
+			todosClientes = clientes.obterTodos();
+			if (todosClientes.isEmpty()){
+				System.out.println("Nenhum cliente encontrado");
+			} else {
+				todosClientes.forEach(System.out::println);
+			}
+		};
 	}
 
 	public static void main(String[] args) {
