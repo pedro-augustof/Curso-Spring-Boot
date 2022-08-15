5d2b99374de8012bc78fbd0a882ddb8aef7c8857
Pedro Augusto <pedro4032@hotmail.com>
2022-08-15 15:22:01 -0300

    Section 8 finished

--- Vendas/src/main/java/com/udemy/cursospring/Vendas/rest/controller/ClienteController.java
+++ Vendas/src/main/java/com/udemy/cursospring/Vendas/rest/controller/ClienteController.java
@@ -2,19 +2,19 @@ package com.udemy.cursospring.Vendas.rest.controller;
 
 import com.udemy.cursospring.Vendas.domain.entity.Cliente;
 import com.udemy.cursospring.Vendas.domain.repository.Clientes;
+import io.swagger.annotations.*;
 import org.springframework.data.domain.Example;
 import org.springframework.data.domain.ExampleMatcher;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 import javax.validation.Valid;
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/clientes")
+@Api("Api Clientes")
 public class ClienteController {
 
     private Clientes clientes;
@@ -24,13 +24,23 @@ public class ClienteController {
     }
 
     @GetMapping("/{id}")
-    public Cliente getClienteById(@PathVariable Integer id){
+    @ApiOperation("Obter detalhes de um cliente")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "Cliente encontrado"),
+            @ApiResponse(code = 404, message = "Cliente não encontrado para o ID informado")
+    })
+    public Cliente getClienteById(@PathVariable @ApiParam("Id do cliente") Integer id){
         return clientes.findById(id).orElseThrow( () -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Cliente não encontrado"));
 
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
+    @ApiOperation("Salva um novo cliente")
+    @ApiResponses({
+            @ApiResponse(code = 201, message = "Cliente salvo com sucesso"),
+            @ApiResponse(code = 400, message = "Erro de validação")
+    })
     public Cliente save (@RequestBody @Valid Cliente cliente){
         return clientes.save(cliente);
     }
