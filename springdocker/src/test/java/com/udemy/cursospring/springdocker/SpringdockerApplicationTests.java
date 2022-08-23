39cb7a3fc833865916c4ba06e91cb0990e6ee975
pedro <pedro4032@hotmail.com>
2022-08-23 14:44:35 -0300

    Section 13 finished

---
+++ springdocker/src/test/java/com/udemy/cursospring/springdocker/SpringdockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udemy.cursospring.springdocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringdockerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
